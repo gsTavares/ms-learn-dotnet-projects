07035f81fb658c5ba9284f6af7a668270ddd3b19
Gustavo Tavares <gustavo.tavares24@outlook.com>
2024-02-20 21:24:50 -0300

    feat: arrays and foreach loop

---
+++ CsharpArrays/Program.cs
@@ -0,0 +1,20 @@
+﻿// Code challenge
+
+string[] ids = [
+    "B123",
+    "C234",
+    "A345",
+    "C15",
+    "B177",
+    "G3003",
+    "C235",
+    "B179"
+];
+
+foreach (string id in ids)
+{
+    if (id.ToLower().StartsWith('b'))
+    {
+        Console.WriteLine(id);
+    }
+}
