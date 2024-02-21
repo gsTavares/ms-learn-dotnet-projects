e8839375b4995e92ef9e24eda4e0ad8399f91787
Gustavo Tavares <gustavo.tavares24@outlook.com>
2024-02-20 21:00:53 -0300

    feat: if/else statements

---
+++ ifElse/Program.cs
@@ -0,0 +1,32 @@
+﻿// Code challenge
+
+Random random = new();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration <= 10)
+{
+    if (daysUntilExpiration <= 5)
+    {
+        if (daysUntilExpiration > 1)
+        {
+            discountPercentage = 10;
+            Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save {discountPercentage}%!");
+        }
+        else if (daysUntilExpiration == 1)
+        {
+            discountPercentage = 20;
+            Console.WriteLine($"Your subscription expires within a day!\nRenew now and save {discountPercentage}%!");
+        }
+        else
+        {
+            Console.WriteLine("Your subscription has expired.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Your subscription will expire soon. Renew now!");
+    }
+}
+
+
